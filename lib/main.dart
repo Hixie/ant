fbe98adca8fba23c603c1eddf787ca73462a6d3a
Ian Hickson <ian@hixie.ch>
2020-09-27 14:53:42 -0700

    Visuals

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,246 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
+import 'logic.dart';
+import 'boards.dart' as boards;
+
 void main() {
-  runApp(MyApp());
+  Board board = boards.defaultBoard;
+  runApp(MaterialApp(
+    title: 'Tree Plate Ant',
+    home: BoardView(board: board),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+@immutable
+abstract class CellPainter {
+  factory CellPainter.fromCell(Cell cell) {
+    switch (cell.cellState) {
+      case CellState.empty: return const ColorCellPainter(Colors.white);
+      case CellState.wall: return const ColorCellPainter(Colors.black);
+      case CellState.goal: return const ColorCellPainter(Colors.green);
+      case CellState.marked: return const ColorCellPainter(Colors.yellow);
+      case CellState.start: return const ColorCellPainter(Colors.teal);
+      case CellState.walked: return ColorCellPainter(Colors.grey.shade100);
+      case CellState.walked2: return ColorCellPainter(Colors.grey.shade500);
+    }
+    throw UnsupportedError('cannot handle $cell');
+  }
+
+  const CellPainter._();
+
+  void paint(Canvas canvas, Rect rect);
+
+  static CellPainter lerp(CellPainter a, CellPainter b, double t) {
+    assert(t != null);
+    assert(a != null);
+    assert(b != null);
+    if (t == 0.0) {
+      return a;
+    }
+    if (t == 1.0) {
+      return b;
+    }
+    return b.lerpFrom(a, t);
+  }
+
+  @protected
+  CellPainter lerpFrom(CellPainter a, double t) => t < 0.5 ? a : this;
+}
+
+class ColorCellPainter extends CellPainter {
+  const ColorCellPainter(this.color) : super._();
+
+  final Color color;
+
+  void paint(Canvas canvas, Rect rect) {
+    canvas.drawRect(rect, Paint()..color = color);
+  }
+
+  CellPainter lerpFrom(CellPainter a, double t) {
+    if (a is ColorCellPainter) {
+      return ColorCellPainter(Color.lerp(a.color, this.color, t));
+    }
+    return super.lerpFrom(a, t);
+  }
+}
+
+@immutable
+class BoardPainter extends CustomPainter {
+  factory BoardPainter.fromBoard(Board board) {
+    return BoardPainter._(
+      board.width,
+      board.currentState
+          .map<CellPainter>((Cell cell) => CellPainter.fromCell(cell))
+          .toList(),
+      Offset(board.antX + 0.5, board.antY + 0.5),
+    );
+  }
+
+  const BoardPainter._(this.width, this.cells, this.antOffset);
+
+  final int width;
+  int get height => cells.length ~/ width;
+  final List<CellPainter> cells;
+  final Offset antOffset;
+
+  void paint(Canvas canvas, Size size) {
+    for (int y = 0; y < height; y += 1) {
+      for (int x = 0; x < width; x += 1) {
+        cells[x + y * width].paint(canvas, Rect.fromLTWH(x.toDouble(), y.toDouble(), 1.0, 1.0));
+      }
+    }
+    canvas.drawCircle(
+      antOffset,
+      0.5,
+      Paint()..color = Colors.red,
+    );
+  }
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+  bool shouldRepaint(BoardPainter oldDelegate) {
+    return width != oldDelegate.width
+        || cells != oldDelegate.cells
+        || antOffset != oldDelegate.antOffset;
+  }
+
+  static BoardPainter lerp(BoardPainter a, BoardPainter b, double t) {
+    assert(t != null);
+    assert(a != null);
+    assert(b != null);
+    if (t == 0.0) {
+      return a;
+    }
+    if (t == 1.0) {
+      return b;
+    }
+    assert(a.width == b.width);
+    assert(a.cells.length == b.cells.length);
+    return BoardPainter._(
+      b.width,
+      List<CellPainter>.generate(a.cells.length, (int index) => CellPainter.lerp(a.cells[index], b.cells[index], t)),
+      Offset.lerp(a.antOffset, b.antOffset, t),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+class BoardPainterTween extends Tween<BoardPainter> {
+  BoardPainterTween({ BoardPainter begin, BoardPainter end }) : super(begin: begin, end: end);
+  
+  BoardPainter lerp(double t) {
+    return BoardPainter.lerp(begin, end, t);
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class BoardView extends StatefulWidget {
+  BoardView({Key key, this.board}) : super(key: key);
 
-  void _incrementCounter() {
+  final Board board;
+
+  @override
+  _BoardViewState createState() => _BoardViewState();
+}
+
+class _BoardViewState extends State<BoardView> {
+  BoardPainter _currentPainter;
+
+  @override
+  void initState() {
+    super.initState();
+    _update();
+    _toggle();
+  }
+
+  void didUpdateWidget(BoardView oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.board != widget.board) {
+      _update();
+    }
+  }
+
+  void _update() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentPainter = BoardPainter.fromBoard(widget.board);
     });
+  }
+
+  Timer _timer;
+
+  void _toggle() {
+    if (_timer == null) {
+      _timer = Timer.periodic(const Duration(milliseconds: 750), _advance);
+    } else {
+      _timer.cancel();
+      _timer = null;
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  void _advance([Timer timer]) {
+    widget.board.advance();
+    _update();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return SafeArea(
+      child: GestureDetector(
+        onTap: _toggle,
+        child: FittedBox(
+          child: SizedBox(
+            width: widget.board.width.toDouble(),
+            height: widget.board.height.toDouble(),
+            child: AnimatedBoardCanvas(
+              duration: const Duration(milliseconds: 150),
+              curve: Curves.easeInQuint,
+              board: _currentPainter,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class AnimatedBoardCanvas extends ImplicitlyAnimatedWidget {
+  AnimatedBoardCanvas({
+    Key key,
+    this.board,
+    Curve curve: Curves.linear,
+    @required Duration duration,
+    VoidCallback onEnd,
+    this.child,
+  }) : super(key: key, curve: curve, duration: duration, onEnd: onEnd);
+
+  final BoardPainter board;
+  final Widget child;
+
+  @override
+  _AnimatedBoardCanvasState createState() => _AnimatedBoardCanvasState();
+}
+
+class _AnimatedBoardCanvasState extends AnimatedWidgetBaseState<AnimatedBoardCanvas> {
+  Tween<BoardPainter> _board;
+
+  @override
+  void forEachTween(TweenVisitor<dynamic> visitor) {
+    _board = visitor(
+      _board,
+      widget.board, (dynamic value) => BoardPainterTween(begin: widget.board),
+    ) as Tween<BoardPainter>;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: _board.evaluate(animation),
+      child: widget.child,
     );
   }
 }
